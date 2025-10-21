ec36ddeceb6833ecefe55eb01dd36da83c1dc202
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2025-10-21 17:04:58 -0500

    implementacion de la importacion de datos 1/2

--- app/Livewire/Superadmin/Collaborator/CollaboratorImport.php
+++ app/Livewire/Superadmin/Collaborator/CollaboratorImport.php
@@ -4,13 +4,45 @@ namespace App\Livewire\Superadmin\Collaborator;
 
 use Livewire\Component;
 use Spatie\SimpleExcel\SimpleExcelReader;
+use Illuminate\Support\Collection;
+use Livewire\WithFileUploads;
 
 class CollaboratorImport extends Component
 {
-    public $dataExcel;
+    use WithFileUploads;
+    public $excel;
+    public Collection $errorsImport;
+    public $preview = [];
+
+    protected $rules = [
+        'excel' => 'required|file|mimes:xlsx,xls,csv|max:5120', // Max size 2MB
+    ];
+
+    public function mount()
+    {
+        $this->errorsImport = collect();
+    }
 
     public function importExcel()
     {
+        $this->validate();
+
+        $path = $this->excel->getRealPath();
+
+        $this->preview = SimpleExcelReader::create($this->excel->getRealPath())
+            ->useHeaders(['Ciudad', 'Codigo', 'Nombres', 'Identificacion', 'Cargo', 'Jefe', 'CC', 'Empresa'])
+            ->getRows()
+            ->take(20)
+            ->toArray();
+
+//         SimpleExcelReader::create($this->file)
+//             ->useHeaders(['Ciudad', 'Codigo', 'Nombres', 'Identificacion
+// ', 'Cargo', 'Jefe', 'CC', 'Empresa'])
+//             ->getRows()
+//             ->each(function (array $rowProperties) {
+//                 // Process each row as needed
+//                 dump($rowProperties);
+//             });
 
     }
 
--- resources/views/livewire/superadmin/collaborator/collaborator-import.blade.php
+++ resources/views/livewire/superadmin/collaborator/collaborator-import.blade.php
@@ -13,24 +13,65 @@
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                     <div class="bg-gray-800 text-gray-100 p-6 rounded-lg shadow-xl mt-4 mb-4">
                         <h2 class="text-center mb-3">Importacion de colaboradores desde Excel</h2>
-                        <x-input type="file" />
-                        <x-button>Cargar archivo</x-button>
-                        <x-button >Guardar Importacion</x-button>
+
+                        <x-form-section wire:submit.prevent="importExcel" enctype="multipart/form-data">
+                            <x-slot name="title">Importacion de datos</x-slot>
+                            <x-slot name="description">
+                                    Seleccione el archivo Excel (.xlsx) que contiene los datos de los colaboradores a importar. Asegúrese de que el formato del archivo sea correcto.
+                            </x-slot>
+                            <div class="mb-4">
+                                <x-label for="excel" value="Archivo Excel.xlxs"/>
+                                <x-input id="excel" type="file" wire:model="excel" class="mt-1 w-full"/>
+                                <x-input-error for="excel" class="mt-2"/>
+                            </div>
+                            <x-slot name="buttonText">Importar</x-slot>
+                        </x-form-section>
+                        @if($errorsImport->isNotEmpty())
+                            <div class="mt-4 text-red-600 text-sm">
+                                <p>Algunas filas no se pudieron importar:</p>
+                                <ul class="list-disc ml-6">
+                                    @foreach($errorsImport as $e)
+                                        <li>Fila {{ $e['row'] }}: {{ $e['msg']}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
                     </div>
                 </div>
             </div>
-
+            @if($preview)
             <section>
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                         <div class="bg-gray-900 text-gray-100 p-6 rounded-lg shadow-xl">
                             <div class="overflow-x-auto">
                                 <!-- tabla de colaboradores -->
-
+                                <x-data-table :data="$preview" class="mt-4">
+                                    <x-slot name="headers">
+                                        <tr class="bg-gray-800 text-gray-100">
+                                            <th class="px-4 py-2">Nombres</th>
+                                            <th class="px-4 py-2">Apellidos</th>
+                                            <th class="px-4 py-2">Identificacion</th>
+                                            <th class="px-4 py-2">Codigo de nomina</th>
+                                            <th class="px-4 py-2">Area</th>
+                                            <th class="px-4 py-2">Cargo</th>
+                                            <th class="px-4 py-2">Regional</th>
+                                        </tr>
+                                    </x-slot>
+                                    <x-slot name="dataTBody">
+                                        @foreach($row as $v)
+                                            <tr class="border-b border-gray-700">
+                                                <td class="px-4 py-2">{{ $v }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </x-slot>
+                                </x-data-table>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            @endif
     </div>
 </div>
