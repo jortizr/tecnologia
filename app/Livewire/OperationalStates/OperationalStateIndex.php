d74cbfd683d4675069e1d5d69e6b5f950cd8197d
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-02-18 16:06:22 -0500

    creacion del componente y politicas de seguridad para el modelo estados operacionales

---
+++ app/Livewire/OperationalStates/OperationalStateIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\OperationalStates;
+
+use Livewire\Component;
+
+class OperationalStateIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.operational-states.operational-state-index');
+    }
+}
