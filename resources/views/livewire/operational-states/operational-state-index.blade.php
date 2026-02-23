7dd1c86cfee826be68462ebc99ee89166f8569ef
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-02-22 23:04:06 -0500

    Agregar estructura y estilo a la vista de gestión de estados operacionales

--- resources/views/livewire/operational-states/operational-state-index.blade.php
+++ resources/views/livewire/operational-states/operational-state-index.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div  class="py-6">
+    <div class="flex justify-center items-center">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Gestion de Estados Operacionales') }}
+        </h2>
+        <x-badge-title :count="$this->operationalStates->total()" />
+    </div>
 </div>
